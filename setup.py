ab693a8559c371488f23976b2c21ded0e0ace39e
tehzevo <tehzevo@users.noreply.github.com>
2019-09-05 20:32:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(name='pget',
   version='0.1.0',
   install_requires = [
     'ml_utils @ git+https://git@github.com/tehzevo/ml-utils@master#egg=ml_utils',
+    'tensorflow',
+    'numpy'
   ],
   packages=find_packages())
