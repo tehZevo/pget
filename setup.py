97c47a56ea649fb2555b7cb2cafa724bc25915a5
tehZevo <tehzevo@users.noreply.github.com>
2019-10-23 22:32:01 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pget',
-  version='0.2.0',
+  version='0.3.0',
   install_requires = [
     'ml_utils @ git+https://git@github.com/tehzevo/ml-utils@master#egg=ml_utils',
     'tensorflow',
